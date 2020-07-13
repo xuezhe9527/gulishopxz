d959c59157376b2d0e0211198b0f8b1dbd7d1abc
xuezhe <18202969264@163.com>
2020-07-13 20:23:09 +0800

    最终最终day02

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,32 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 import routes from '@/router/routes'
+
+//终极解决多次触发push或者repalce，报错的问题
+//NavigationDuplicated
+
+const originPush = VueRouter.prototype.push
+const originReplace = VueRouter.prototype.replace
+
+VueRouter.prototype.push = function(location,onResolved,onRejected){
+  if(onResolved === undefined && onRejected === undefined){
+    // originPush.call目的是让VueRouter实例化对象去调用‘
+    //如果不加，那就是window在调用
+    return originPush.call(this,location).catch(() => {})
+  }else{
+    return originPush.call(this,location,onResolved,onRejected)
+  }
+}
+
+VueRouter.prototype.replace = function(location,onResolved,onRejected){
+  if(onResolved === undefined && onRejected === undefined){
+    // originPush.call目的是让VueRouter实例化对象去调用‘
+    //如果不加，那就是window在调用
+    return originReplace.call(this,location).catch(() => {})
+  }else{
+    return originReplace.call(this,location,onResolved,onRejected)
+  }
+}
 export default new VueRouter({
   routes
 })
